333dc7aeda80f58f989416edc17342a234ff9ac5
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2020-02-05 13:18:44 -0800

    Cleaned up code and added praying to shrine and checking for wishing well. We will need to remove the wishing well

--- src/functions.py
+++ src/functions.py
@@ -2,6 +2,17 @@ import requests
 import json
 import time
 
+
+def instantiate():
+    headers = {
+    'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
+        }
+
+    response = requests.get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers=headers)
+
+    time.sleep(response.json()["cooldown"])
+    return response.json()
+
 def inventAndCoins():
     headers = {
     'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
@@ -16,49 +27,32 @@ def inventAndCoins():
 #         inventory=inventAndCoins()
 #     return inventory["strength"]
 def InventoryList(inventory = None):
-    if inventory is None:
-        inventory=inventAndCoins()
+    if inventory==None:
+        inventory=inventAndCoins() 
     return inventory["inventory"]
+
 def InventoryLimitReached(inventory = None):
-    if inventory is None:
-        inventory=inventAndCoins()
+    inventory=inventAndCoins()
     if len(inventory["inventory"])==inventory["strength"]:
         return True
     else:
         return False
-def coinsNeeded(inventory = None):
-    if inventory is None:
-        inventory=inventAndCoins()
 
+def coinsNeeded(inventory = None):
+    inventory=inventAndCoins()
     if inventory["gold"] >=1000:
         return True
     else:
         return False
-# def coinsCarried():
-#     Inventory=inventAndCoins()
-#     return Inventory.gold
 
 def sellAndConfirm(treasure):
-    # headers = {
-    # 'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
-    # 'Content-Type': 'application/json',
-    # }
-
-    # data = '{"name":f"{treasure}"}'
-
-    # response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/sell/', headers=headers, data=data)
-    # print(response.json().messages)
     headers = {
-    'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
-    'Content-Type': 'application/json',
-    }
-
+        'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
+        'Content-Type': 'application/json',}
     data = '{"name":"' +str(treasure)+'", "confirm":"yes"}'
-
     response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/sell/', headers=headers, data=data)
     print("treasure sold", response.json())
     time.sleep(response.json()["cooldown"])
-
 
 def pickUpTreasure(treasure):
     headers = {
@@ -67,8 +61,7 @@ def pickUpTreasure(treasure):
     }
 
     data ='{"name":"' +str(treasure)+ '"}'
-
-    response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/take/', headers=headers, data=data)
+    response =requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/take/', headers=headers, data=data)
     time.sleep(response.json()["cooldown"])
     return response.json
 
@@ -105,3 +98,23 @@ def dropEquipment(Equipment):
 
     response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/undress/', headers=headers, data=data)
     print("Drop Equipment", response.json())
+
+def changeName():
+    headers={
+    'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
+    'Content-Type': 'application/json',
+    }
+
+    data = '{"name":"[Obaida Albaroudi]"}'
+
+    response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/change_name/', headers=headers, data=data)
+    print("Change Name",response.json())
+
+def shrinePray():
+    headers = {
+    'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
+    'Content-Type': 'application/json',
+    }
+
+    response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/pray/', headers=headers)
+    print("Shrine Pray", response.json())
--- src/traversal.py
+++ src/traversal.py
@@ -2,34 +2,37 @@ from util import Graph,Stack,Queue
 import requests 
 import json
 import time
-from functions import inventAndCoins,pickUpTreasure,InventoryLimitReached, InventoryList, sellAndConfirm
+from functions import instantiate, inventAndCoins, pickUpTreasure, InventoryLimitReached, InventoryList, sellAndConfirm, coinsNeeded,changeName, shrinePray
 
 """
-    Take into Account:
+    To-Do:
+        - Traversal path (Big Array) ==>will need to hold the data from the rooms, online SQL. [tonight]
+        - Note where the wishing well is. [Need to figure this out]
+        - Wishing well => Change name
+        - mine coin (blockchain, hashtable)
+        - Nice Additions:
+            - Shrine to get abilities:
+                - Fly
+                - Dash
+            - Pick up equipment & Drop
+            - Ghost thing
+    
+    - DFT => Traverse [WIP]
+    - BFS => Shortest paths [WIP]
+
+    DONE:
         - Cool Down Time [DONE]
         - Inventory (limit of how many items we can carry) [DONE]
             - treasure or other items that could help in the rooms
         - 1,000+ coins go to the wishing well (if we know where it is, if not search for it).
-          Once found do action there (change name) [tonight]
-
-    Directions:
-        - Traversal path (Big Array) ==>will need to hold the data from the rooms, online SQL. [tonight]
-
+          Once found do action there (change name) [Done]
         - Note where the shop is to sell the treasure => BFS back to here everytime
-          our inventory is full or over a limit we set (70%)
-        - Note where the wishing well is.
-
-
+          our inventory is full or over a limit we set (70%) [Done]
         - Once we hit a dead end go back to the nearest room that has not been
           visited. [Done]
-
-    
-    Information:
         - Add new rooms to graphs with the edges  [Done]
         - Save information in the other vertex in the graph [Done]
-    
-    - DFT => Traverse [WIP]
-    - BFS => Shortest paths [WIP]
+
 
     
 """
@@ -71,65 +74,94 @@ def wise_move(direct, next_room_id):
 
 
 def traverse(startingRoom):
-    # print("startingRoom",startingRoom,startingRoom["room_id"])
-    stack = Stack() # Will hold path
+    #startingRoom represents the room we are currently in as a player. It is coming from the instantiation function in the function file.
+
+
+    # Will hold path so we can push and pop.
+    stack = Stack() 
+
     # path=[] # Will hold room id
-    additional_option = Stack() # Will hold room id of rooms with unvisited directions
-    visited = set() # Will hold the ids of the rooms we visited
-    stack.push(startingRoom["room_id"])
+    # Will hold room id of rooms with unvisited directions so we can traverse back to them once we hit a dead end.
+    additional_option = Stack() 
+
+    # Will hold the ids of the rooms we visited so we can make sure we hit 500 unique rooms then break.
+    visited = set() 
+
+    #Instantiates and gives us our first room in the stack.
+    stack.push(startingRoom["room_id"]) 
+
+    
     graph.add_vertex(startingRoom["room_id"], startingRoom["title"], startingRoom["description"], startingRoom["coordinates"], startingRoom["elevation"], startingRoom["terrain"], startingRoom["players"], startingRoom["items"], startingRoom["exits"], startingRoom["cooldown"], startingRoom["errors"], startingRoom["messages"])
+
     shop = None
+
+    wishingWell=None
+
     if startingRoom["title"] == "Shop":
         shop = startingRoom["room_id"]
+
+    if startingRoom["title"] == "Wishing Well":
+        wishingWell = startingRoom["room_id"]
+
     while stack.size() > 0:
+        print("Player Stats At The Moment",inventAndCoins())
 
         room_id = stack.pop()
-        print("\n\nCurrent room_id",room_id, "shop", shop)
-        # print(f"\n inventAndCoins()",inventAndCoins())
 
         if room_id not in visited:
             print(f"Room {room_id} added to visited")
             visited.add(room_id)
-            # graph.add_vertex(room_id, title, description, coordinates, elevation, terrain, players, items, exits, cooldown, errors, messages)
-            # path.append(room_id)
+
         if len(visited) == 500:
             # return path
             return
         exits = graph.directions[room_id]
-        # print("EXITS", exits)
-        # potential_rooms.sort()
         possible_directions = 0
         moved=False
         for key in exits:
-            # print("Breaks Here 1")
+            
+            #This handle an unexplored room thus "?"
             if exits[key] =="?":
-                # print("Breaks Here 4",)
-                # print("key",key,"exits[key]",exits[key],"current Room", room_id, graph.directions[room_id])
+
+                #possible_directions adds the amount of question marks per direction a node has
                 possible_directions +=1
+
+                #This moves us to one of the rooms that is unexplored and adds it to the vertices and adds the edges.
                 if moved==False:
-                    print(f"Moving from {room_id}, direction {key}")
                     nextRoom=move(key)
-                    print("\nNow in nextRoom",nextRoom)
                     
                     graph.add_vertex(nextRoom["room_id"], nextRoom["title"], nextRoom["description"], nextRoom["coordinates"], nextRoom["elevation"], nextRoom["terrain"], nextRoom["players"], nextRoom["items"], nextRoom["exits"], nextRoom["cooldown"], nextRoom["errors"], nextRoom["messages"])
-                    # print("graph.directions",graph.directions)
                     graph.add_edge(room_id,key,nextRoom["room_id"])
-                    # print("graph.directions After",graph.directions)
+
+                    #Checks to see if the room title equals shop and if so we make the variable shop equal to the id
                     if nextRoom["title"] == "Shop":
                         shop = nextRoom["room_id"]
+
+                    #Checks to see if the room title equals Wishing Well and if so we make the variable wishingWell equal to the id
+                    if nextRoom["title"] == "Wishing Well":
+                        wishingWell = nextRoom["room_id"]
+                    
+                    #Checks to see if room is a shrine. If it is we pray to get special powers.
+                    if nextRoom["title"] == "Shrine":
+                        shrinePray()
                     
                     moved=True
+
+                    #If the next room (the room we have just moved into) has items then we will for loop and pick up the items
                     if len(nextRoom["items"])>0:
-                        # print("\nInventoryLimitReached",InventoryLimitReached())
+
+                        #Checks to see if we are carrying more than our limit or not. If we are not then we pick up items
                         if InventoryLimitReached()==False:
                             for item in nextRoom["items"]:
-                                print(f"about to pick up {item}")
+                                # if item == "Small Treasure":
+                                print("ITEM", item)
                                 pickUpTreasure(item)
+                        #If we reached the limit of items we can carry and we know the shop id because we discovered it we then bfs to the shop to sell our treasure and traverse back to the nextRoom id as that was the last room we were in. The functionality is similar to what we did with the bfs when possible_directions===0
                         elif shop is not None:
                             path_to_shop = graph.bfs(nextRoom["room_id"], shop)
                             reverse = path_to_shop[::-1]
+                            reverse=reverse[1:]
                             total_path = path_to_shop.extend(reverse)
-                            # path.extend(total_path[1:])
                             i=0
 
                             next_room_id = None
@@ -138,45 +170,38 @@ def traverse(startingRoom):
 
                                 current_room_id = total_path[i]
                                 next_room_id = total_path[i+1]
-                                print("CURRENT ROOM", current_room_id)
-                                print("NEXT ROOM", next_room_id)
+
+                                #Checks to see if the current room we are in is the one that belongs to the shop. If it is we loop through our inventory and sell our treasure.
                                 if current_room_id ==shop:
                                     inventory_list = InventoryList()
                                     for item in inventory_list:
                                         sellAndConfirm(item)
 
-                            # for index, item in enumerate(path_to_room):
-                                # print("Item in path_to_room", )
                                 direction=graph.directions[current_room_id]
-                                # direction=graph.directions[room_id].keys(item)
-                                # move(direction)
-                                print(f"DIRECTION: {direction}")
+
                                 for key in direction:
-                                    # print("Key in direction", key,direction[key],item, graph.directions[item].values())
                                     if direction[key] == next_room_id:
-                                        print(f"about to make a WISE MOVE from {current_room_id} {key} to {next_room_id}")
-                                        next_room = wise_move(key, next_room_id)
-                                        print(f"\nMade a WISE MOVE to {next_room}")                    
+                                        next_room = wise_move(key, next_room_id)                 
                                 if current_room_id not in visited:
                                     visited.add(item)
                                 i+=1
+                    #Check to see if we have the 1,000 plus coins we need to purchase a new name.If we do and we know where the wishing well is we bfs there and change our name so we can mine. 
+                    if coinsNeeded()==True:
+                        if wishingWell is not None:
+                            #NEED TO TRAVERSE TO wishingWell
+                            changeName()
+
+
                     stack.push(nextRoom["room_id"])        
 
-                
-            # print("Breaks Here 5")
+        #This adds a room id to additional_option since it has a direction with a question mark
         if possible_directions > 1:
-            print(f"More than 1 possible direction, adding {room_id} to additional option stack")
             additional_option.push(room_id)
-        #Another if to check for Inventory limit
-        #Another if to check for treasure limit
+
+        #Below part triggers BFS since we now hit a dead end.
         if possible_directions == 0:
             return_room = additional_option.pop()
-            print(f"No unknown directions, going back to {return_room}")
-            # print("next_room",next_room, "room id",room_id)
             path_to_room = graph.bfs(room_id, return_room)
-            print("Following path_to_room", path_to_room)
-    
-            # path.extend(path_to_room[1:])
 
             i=0
             next_room_id = None
@@ -184,42 +209,22 @@ def traverse(startingRoom):
                 
                 current_room_id = path_to_room[i]
                 next_room_id = path_to_room[i+1]
-                print("CURRENT ROOM", current_room_id)
-                print("NEXT ROOM", next_room_id)
 
-            # for index, item in enumerate(path_to_room):
-                # print("Item in path_to_room", )
                 direction=graph.directions[current_room_id]
-                # direction=graph.directions[room_id].keys(item)
-                # move(direction)
-                print(f"DIRECTION: {direction}")
+
                 for key in direction:
-                    # print("Key in direction", key,direction[key], graph.directions[current_room_id].values())
                     if direction[key] == next_room_id:
-                        print(f"about to make a WISE MOVE from {current_room_id} {key} to {next_room_id}")
-                        next_room = wise_move(key, next_room_id)
-                        print(f"\nMade a WISE MOVE to {next_room}")                    
+                        next_room = wise_move(key, next_room_id)               
                 if current_room_id not in visited:
                     visited.add(item)
                 i+=1
-            # print("End of path",is not in list path[-1], "Instantiate",instantiate()["room_id"])
             stack.push(next_room_id)
-        print("graph vertices", graph.directions)
+        print("These are the rooms we have visited with directions\n", graph.directions)
     return None
 
-
-def instantiate():
-    headers = {
-    'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
-        }
-
-    response = requests.get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers=headers)
-
-    time.sleep(response.json()["cooldown"])
-    return response.json()
 
 count = 0
 traverse(instantiate())
 
-print(graph.directions)
+# print(graph.directions)
 
