06032eee3ab645fa14006ed18726bec715486de8
Shannon <shannon.yoshikawa@gmail.com>
2020-02-06 08:27:14 -0800

    adjusted traversal to optomize rooms that we have directions for

--- src/functions.py
+++ src/functions.py
@@ -3,9 +3,10 @@ import json
 import time
 
 
+
 def instantiate():
     headers = {
-    'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
+    'Authorization': 'Token 6b62daaa5f05f9ea8b0b1b77a7a9a5d7f4101edb',
         }
 
     response = requests.get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers=headers)
@@ -15,12 +16,16 @@ def instantiate():
 
 def inventAndCoins():
     headers = {
-    'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
+    'Authorization': 'Token 6b62daaa5f05f9ea8b0b1b77a7a9a5d7f4101edb',
     'Content-Type': 'application/json',
     }
+    try:
+        response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/status/', headers=headers)
+        return response.json()
+    except:
+        # print("Room_info", graph.room_info, "\n\nDirections", graph.directions)
+        return
 
-    response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/status/', headers=headers)
-    return response.json()
 
 # def CarryLimit(inventory = None):
 #     if inventory is None:
@@ -45,41 +50,61 @@ def coinsNeeded(inventory = None):
     else:
         return False
 
-def sellAndConfirm(treasure):
+def sellAndConfirm(treasure, graph):
     headers = {
-        'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
+        'Authorization': 'Token 6b62daaa5f05f9ea8b0b1b77a7a9a5d7f4101edb',
         'Content-Type': 'application/json',}
-    data = '{"name":"' +str(treasure)+'", "confirm":"yes"}'
-    response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/sell/', headers=headers, data=data)
-    print("treasure sold", response.json())
-    time.sleep(response.json()["cooldown"])
+    data = '{"name":"' +str(treasure)+'"}'
+    # selling
+    try:
+        response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/sell/', headers=headers, data=data)
+        print("treasure sold", response.json())
+        time.sleep(response.json()["cooldown"])
+    except:
+        print("Room_info", graph.room_info, "\n\nDirections", graph.directions)
+        return
+    try:    
+        data = '{"name":"' +str(treasure)+'", "confirm":"yes"}'
+        response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/sell/', headers=headers, data=data)
+        time.sleep(response.json()["cooldown"])
+    except:
+        print("Room_info", graph.room_info, "\n\nDirections", graph.directions)
+        return
 
-def pickUpTreasure(treasure):
+
+def pickUpTreasure(treasure, graph):
     headers = {
-    'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
+    'Authorization': 'Token 6b62daaa5f05f9ea8b0b1b77a7a9a5d7f4101edb',
     'Content-Type': 'application/json',
     }
 
     data ='{"name":"' +str(treasure)+ '"}'
-    response =requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/take/', headers=headers, data=data)
-    time.sleep(response.json()["cooldown"])
-    return response.json
+    try:
+        response =requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/take/', headers=headers, data=data)
+        time.sleep(response.json()["cooldown"])
+        return response.json
+    except:
+        print("Room_info", graph.room_info, "\n\nDirections", graph.directions)
+        return
 
-def dropTreasure(treasure):
+def dropTreasure(treasure, graph):
     headers = {
-    'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
+    'Authorization': 'Token 6b62daaa5f05f9ea8b0b1b77a7a9a5d7f4101edb',
     'Content-Type': 'application/json',
     }
 
     data = '{"name":f"{treasure}"}'
-
-    response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/drop/', headers=headers, data=data)
+    try:
+        response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/drop/', headers=headers, data=data)
     
-    return response.json()
+        return response.json()
+    except:
+        print("Room_info", graph.room_info, "\n\nDirections", graph.directions)
+        return 
 
 def wearEquipment(Equipment):
     headers = {
-    'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
+    'Authorization': 'Token 6b62daaa5f05f9ea8b0b1b77a7a9a5d7f4101edb',
     'Content-Type': 'application/json',
     }
 
@@ -90,7 +115,7 @@ def wearEquipment(Equipment):
 
 def dropEquipment(Equipment):
     headers = {
-    'Authorization': 'Token 7a375b52bdc410eebbc878ed3e58b2e94a8cb607',
+    'Authorization': 'Token 6b62daaa5f05f9ea8b0b1b77a7a9a5d7f4101edb',
     'Content-Type': 'application/json',
     }
 
@@ -101,7 +126,7 @@ def dropEquipment(Equipment):
 
 def changeName():
     headers={
-    'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
+    'Authorization': 'Token 6b62daaa5f05f9ea8b0b1b77a7a9a5d7f4101edb',
     'Content-Type': 'application/json',
     }
 
@@ -110,11 +135,14 @@ def changeName():
     response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/change_name/', headers=headers, data=data)
     print("Change Name",response.json())
 
-def shrinePray():
+def shrinePray(graph):
     headers = {
-    'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
+    'Authorization': 'Token 6b62daaa5f05f9ea8b0b1b77a7a9a5d7f4101edb',
     'Content-Type': 'application/json',
     }
-
-    response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/pray/', headers=headers)
-    print("Shrine Pray", response.json())
+    try:
+        response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/pray/', headers=headers)
+        print("Shrine Pray", response.json())
+    except:
+        print("Room_info", graph.room_info, "\n\nDirections", graph.directions)
+        return
--- src/traversal.py
+++ src/traversal.py
@@ -38,40 +38,40 @@ import pickle
 
     
 """
+confirmed_directions = {372: {'n': 441, 's': 363}, 441: {'s': 372}, 363: {'n': 372, 'e': 328}, 328: {'n': 332, 's': 312, 'e': 357, 'w': 363}, 332: {'n': 350, 's': 328}, 350: {'n': 436, 's': 332, 'e': 404}, 436: {'s': 350}, 404: {'n': 481, 'w': 350}, 481: {'s': 404}, 312: {'n': 328, 'e': 268}, 268: {'s': 203, 'e': 411, 'w': 312}, 203: {'n': 268, 's': 165, 'e': 299}, 165: {'n': 203, 's': 125, 'w': 204}, 125: {'n': 165, 'e': 83, 'w': '?'}, 83: {'s': 76, 'e': 130, 'w': 125}, 76: {'n': 83, 'e': 72, 'w': '?'}, 72: {'s': 63, 'w': 76}, 63: {'n': 72, 's': 20, 'w': '?'}, 20: {'n': 63, 's': 19, 'e': '?', 'w': '?'}, 19: {'n': 20, 's': 10, 'w': '?'}, 10: {'n': 19, 's': 0, 'w': '?'}, 0: {'n': 10, 's': 2, 'e': '?', 'w': '?'}, 2: {'n': 0, 's': '?', 'e': 3}, 6: {'n': 2, 'w': 7}, 7: {'n': 8, 'e': 6, 'w': 56}, 8: {'s': 7, 'w': 16}, 16: {'n': 58, 'e': 8, 'w': 67}, 58: {'s': 16, 'w': 65}, 65: {'n': 74, 'e': 58, 'w': 139}, 74: {'n': 87, 's': 65, 'w': 161}, 87: {'s': 74}, 161: {'e': 74}, 139: {'e': 65, 'w': 188}, 188: {'e': 139, 'w': 335}, 335: {'e': 188, 'w': 366}, 366: {'e': 335}, 67: {'e': 16, 'w': 162}, 162: {'e': 67}, 56: {'e': 7, 'w': 61}, 61: {'e': 56, 'w': 171}, 171: {'e': 61}, 3: {'s': 9, 'e': 5, 'w': 2}, 9: {'n': 3, 's': 12, 'e': '?'}, 12: {'n': 9, 's': '?', 'e': 14, 'w': '?'}, 18: {'n': 12, 's': 22, 'w': '?'}, 22: {'n': 18, 's': 78, 'w': '?'}, 78: {'n': 22, 's': 108}, 108: {'n': 78, 's': 117, 'e': '?'}, 117: {'n': 108, 's': 131, 'e': '?', 'w': '?'}, 131: {'n': 117, 's': 244, 'w': '?'}, 244: {'n': 131, 'e': 239}, 239: {'n': 198, 'w': 244}, 198: {'n': 166, 's': 239, 'e': '?'}, 166: {'s': 198, 'e': 150, 'w': '?'}, 150: {'n': 135, 'w': 166}, 135: {'s': 150, 'e': 106}, 106: {'n': 100, 's': '?', 'w': 135}, 100: {'s': 106, 'e': 112, 'w': 68}, 112: {'s': 141, 'e': 140, 'w': 100}, 141: {'n': 112, 'e': 156}, 156: {'s': 168, 'e': 164, 'w': 141}, 168: {'n': 156, 'e': 340}, 340: {'w': 168}, 164: {'n': 217, 'e': 298, 'w': 156}, 217: {'s': 164, 'e': 247}, 247: {'e': 261, 'w': 217}, 261: {'s': 277, 'e': 322, 'w': 247}, 277: {'n': 261, 'e': 323}, 323: {'e': 433, 'w': 277}, 433: {'s': 455, 'e': 460, 'w': 323}, 455: {'n': 433}, 460: {'w': 433}, 322: {'n': 382, 'e': 435, 'w': 261}, 382: {'s': 322, 'e': 388}, 388: {'e': 477, 'w': 382}, 477: {'e': 483, 'w': 388}, 483: {'w': 477}, 435: {'w': 322}, 298: {'s': 324, 'w': 164}, 324: {'n': 298, 's': 349, 'e': 354}, 349: {'n': 324, 's': 352, 'e': 384, 'w': 356}, 352: {'n': 349, 's': 362, 'e': 485}, 362: {'n': 352, 's': 399, 'w': 463}, 399: {'n': 362, 's': 467}, 467: {'n': 399}, 463: {'s': 468, 'e': 362}, 468: {'n': 463}, 485: {'w': 352}, 384: {'w': 349}, 356: {'e': 349}, 354: {'w': 324}, 140: {'w': 112}, 68: {'n': 52, 'e': 100}, 52: {'n': 35, 's': 68, 'e': '?'}, 35: {'s': 52, 'w': 34}, 34: {'n': 14, 's': '?', 'e': 35}, 14: {'s': 34, 'e': 37, 'w': 12}, 37: {'w': 14}, 5: {'w': 3}, 130: {'w': 83}, 411: {'w': 268}, 357: {'w': 328}, 299: {'e': 311, 'w': 203}, 311: {'w': 299}, 204: {'n': 219, 'e': 165, 'w': 216}, 219: {'s': 204}, 216: {'n': 234, 'e': 204, 'w': '?'}, 234: {'n': 368, 's': 216, 'w': 252}, 368: {'s': 234}, 252: {'n': 284, 'e': 234}, 284: {'n': 302, 's': 252, 'w': 303}, 302: {'n': 422, 's': 284}, 422: {'n': 426, 's': 302}, 426: {'n': 457, 's': 422}, 457: {'n': 461, 's': 426}, 461: {'s': 457}, 303: {'n': '?', 'e': 284, 'w': '?'}}
 
 graph = Graph()
 def move(direct):
     headers = {
-    'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
+    'Authorization': 'Token 6b62daaa5f05f9ea8b0b1b77a7a9a5d7f4101edb',
     'Content-Type': 'application/json',
     }
 
     data ='{"direction":"' +str(direct)+ '"}'
-
-    response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers=headers, data=data)
+    try:
+        response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers=headers, data=data)
 
     # print(f"Current cooldown time {response.json()}")
-    time.sleep(response.json()["cooldown"])
-    if response.json()["errors"]==True:
-        # print("ERRORRRR",response.json()["errors"])
-        return
-    return response.json()
+        print("Cooldown: ", response.json()["cooldown"], "Errors: ", response.json()["errors"], "Messages: ", response.json()["messages"])
+        time.sleep(response.json()["cooldown"])
+        return response.json()
+    except:
+        print("Room_info", graph.room_info, "\n\nDirections", graph.directions)
 
 def wise_move(direct, next_room_id):
-    # print("inside WISE MOVE, next_room_id", next_room_id)
-    headers = {    'Authorization': 'Token 483f54da97f902a54b1a93b0d6409362f3cf847e',
+    print(f"WISE MOVE: {direct} to {next_room_id}")
+    headers = {    'Authorization': 'Token 6b62daaa5f05f9ea8b0b1b77a7a9a5d7f4101edb',
     'Content-Type': 'application/json',
     }
 
     data = '{"direction":"' +str(direct)+'", "next_room_id": "'+str(next_room_id)+'"}'
-
-    response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers=headers, data=data)
-    
-    time.sleep(response.json()["cooldown"])
-    if response.json()["errors"]==True:
-        # print("ERRORRRR",response.json()["errors"])
-        return
-    return response.json()
+    try:
+        response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers=headers, data=data)
+        print("Cooldown: ", response.json()["cooldown"], "Errors: ", response.json()["errors"], "Messages: ", response.json()["messages"])
+        time.sleep(response.json()["cooldown"])
+        return response.json()
+    except:
+        print("Room_info", graph.room_info, "\n\nDirections", graph.directions)
 
 
 wishingWell=None
@@ -94,20 +94,28 @@ def traverse(startingRoom):
 
     
     graph.add_vertex(startingRoom["room_id"], startingRoom["title"], startingRoom["description"], startingRoom["coordinates"], startingRoom["elevation"], startingRoom["terrain"], startingRoom["players"], startingRoom["items"], startingRoom["exits"], startingRoom["cooldown"], startingRoom["errors"], startingRoom["messages"])
-
+    shrine = None
     shop = None
 
     if startingRoom["title"] == "Shop":
         shop = startingRoom["room_id"]
 
     if startingRoom["title"] == "Wishing Well":
+        global wishingWell
         wishingWell = startingRoom["room_id"]
+    if startingRoom["title"] == "Shrine":
+        shrine = startingRoom["room_id"]
 
     while stack.size() > 0:
+
+        if shop is not None:
+            print("shop", shop)
+        if wishingWell is not None:
+            print("wishingWell", wishingWell)
         # print("Player Stats At The Moment",inventAndCoins())
-
+        # print("\n\ngraph.room_info", graph.room_info)
         room_id = stack.pop()
-
+        print(f"Room {room_id}, # of rooms visited: {len(visited)}")
         if room_id not in visited:
             # print(f"Room {room_id} added to visited")
             visited.add(room_id)
@@ -128,11 +136,14 @@ def traverse(startingRoom):
 
                 #This moves us to one of the rooms that is unexplored and adds it to the vertices and adds the edges.
                 if moved==False:
-                    nextRoom=move(key)
-                    
+                    if room_id in confirmed_directions.keys():
+                        room_directions = confirmed_directions[room_id]
+                        nextRoom = wise_move(key, room_directions[key])
+                    else:
+                        nextRoom=move(key)
                     graph.add_vertex(nextRoom["room_id"], nextRoom["title"], nextRoom["description"], nextRoom["coordinates"], nextRoom["elevation"], nextRoom["terrain"], nextRoom["players"], nextRoom["items"], nextRoom["exits"], nextRoom["cooldown"], nextRoom["errors"], nextRoom["messages"])
                     graph.add_edge(room_id,key,nextRoom["room_id"])
-
+                    print(nextRoom)
                     #Checks to see if the room title equals shop and if so we make the variable shop equal to the id
                     if nextRoom["title"] == "Shop":
                         shop = nextRoom["room_id"]
@@ -143,27 +154,32 @@ def traverse(startingRoom):
                     
                     #Checks to see if room is a shrine. If it is we pray to get special powers.
                     if nextRoom["title"] == "Shrine":
-                        shrinePray()
+                        shrine = nextRoom["room_id"]
+                        shrinePray(graph)
                     
                     moved=True
 
-                    #If the next room (the room we have just moved into) has items then we will for loop and pick up the items
+                    # If the next room (the room we have just moved into) has items then we will for loop and pick up the items
                     if len(nextRoom["items"])>0:
                         if coinsNeeded()==False:
 
-                            #Checks to see if we are carrying more than our limit or not. If we are not then we pick up items
-                            if len(InventoryList())<8:
+                            # Checks to see if we are carrying more than our limit or not. If we are not then we pick up items
+                            inventory = inventAndCoins()
+                            if inventory["encumbrance"]<8:
                                 for item in nextRoom["items"]:
                                     # if item == "Small Treasure":
                                     # print("ITEM", item)
-                                    pickUpTreasure(item)
-                            #If we reached the limit of items we can carry and we know the shop id because we discovered it we then bfs to the shop to sell our treasure and traverse back to the nextRoom id as that was the last room we were in. The functionality is similar to what we did with the bfs when possible_directions===0
+                                    pickUpTreasure(item, graph)
+                                    if len(InventoryList())==8:
+                                        break
+                            # If we reached the limit of items we can carry and we know the shop id because we discovered it we then bfs to the shop to sell our treasure and traverse back to the nextRoom id as that was the last room we were in. The functionality is similar to what we did with the bfs when possible_directions===0
                             elif shop is not None:
+                                print(shop)
                                 if InventoryLimitReached()==False:
                                     for item in nextRoom["items"]:
                                         # if item == "Small Treasure":
                                         # print("ITEM", item)
-                                        pickUpTreasure(item)
+                                        pickUpTreasure(item, graph)
                                         if len(InventoryList())==10:
                                             break
                                 if InventoryLimitReached()==True:
@@ -184,7 +200,7 @@ def traverse(startingRoom):
                                     if current_room_id ==shop:
                                         inventory_list = InventoryList()
                                         for item in inventory_list:
-                                            sellAndConfirm(item)
+                                            sellAndConfirm(item, graph)
 
                                     direction=graph.directions[current_room_id]
 
@@ -224,7 +240,7 @@ def traverse(startingRoom):
                     visited.add(item)
                 i+=1
             stack.push(next_room_id)
-        # print("These are the rooms we have visited with directions\n", graph.room_info)
+        print("directions\n", graph.directions)
     return None
 
 
